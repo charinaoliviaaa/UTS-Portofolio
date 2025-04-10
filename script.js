eeeb0d1ac38822e63908cd702c2c27dba58fe51a
Charina <olivia0812t@gmail.com>
2025-04-10 07:01:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".skills", {
+    strings: ["Student", "Data Analyst", "Frontend Developer", "Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+  
